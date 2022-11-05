db86c32ff0eccc4281a30962d9fadffa597ed153
DEEPAKKUMARMAHASETH <deepakkumarmahaseth5@gmail.com>
2022-11-05 11:26:27 +0530

    i have also fixed the code please let me know

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 let num = 13;
-
-for(let i = 1; i<num; i++){
+let sum = 0;
+for(let i = 2; i<num; i++){
 	if(num%i==0){
 	count++;
 }
 }
-if(count==0){
+if(sum==0){
 console.log("Prime");
 }else{
 console.log("not a prime");
